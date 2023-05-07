1b03a02bb99ae683a14fbe5e28d67fb91436cf9b
hansimadavane <hansizacarias.madavane@gmail.com>
2023-05-07 22:45:57 +0200

    Mensagem do commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 #from django.http import HttpResponse
 
@@ -9,6 +10,14 @@ def home(request):
     context = {'posts': posts}
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
