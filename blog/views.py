b0710c3d5ffb12c9a54042c84e7474a49d1f66aa
hansimadavane <hansizacarias.madavane@gmail.com>
2023-05-09 15:33:49 +0200

    last update

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 #from django.http import HttpResponse
 
@@ -19,6 +20,31 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
